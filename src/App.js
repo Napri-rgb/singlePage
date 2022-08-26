abc137439b61ab15a26900ea107a3e27812a428d
Napri <manishhh821@gmail.com>
2022-08-26 21:02:16 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import List from "./Screens/list";
+import Upload from "./Screens/upload";
+import Login from "./Screens/login";
+import { useEffect, useState } from "react";
+import Header from "./components/header";
 function App() {
+  const [token, setToken] = useState();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) setToken(token);
+  }, []);
+  if (!token) return <Login setToken={setToken} />;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/" element={<List />} />
+        <Route path="/upload" element={<Upload />} />
+        {/* <Route path="/profile" element={<List />} />
+        <Route path="/upload" element={<List />} /> */}
+      </Routes>
     </div>
   );
 }
---
+++ src/Screens/list.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function List() {
+  <div
+  style={{
+    display: "flex",
+    flexDirection: "row",
+    backgroundColor: 'red',
+    height: 200,
+    width: "100%",
+  }}
+  >
+    kkk
+    <div>Hello</div>
+    <div>ff</div>
+    <div>dsd</div>
+    <div>dsds</div>
+  </div>
+}
+
+export default List;
